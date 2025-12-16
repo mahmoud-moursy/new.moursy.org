e9bbc1bf45cc1b06c4cade7cfc9501f339ad74c5
Mahmoud Moursy <bageliq@protonmail.com>
2025-12-16 20:16:49 +0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig, fontProviders} from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [
+      {
+        name: 'Ancizar Serif',
+        cssVariable: '--ancizar-serif',
+        provider: fontProviders.google(),
+        weights: [400, 600, 900]
+      }
+    ]
+  }
+});
