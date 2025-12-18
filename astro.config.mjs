3a80e5f38c3013aa384cf4059a3976ba10fa43fc
Mahmoud Moursy <bageliq@protonmail.com>
2025-12-18 23:15:05 +0400

    Polish up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,42 @@ import tailwindcss from '@tailwindcss/vite';
 
 
 
+import node from "@astrojs/node";
+
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://moursy.org",
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    ssr: {
+      external: ["node:events"]
+    }
   },
+
+  server: {
+    host: true,
+  },
+
   output: "server",
+
   experimental: {
     fonts: [
       {
         name: 'Ancizar Serif',
         cssVariable: '--ancizar-serif',
-        provider: fontProviders.google(),
-        weights: [400, 600, 900]
+        provider: fontProviders.fontsource(),
+        weights: [400, 600, 900],
+        fallbacks: ["serif"]
       }
-    ]
-  }
+    ],
+    csp: true,
+    svgo: true,
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
